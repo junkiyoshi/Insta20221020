ecb56d63eee79316511e0784c3dc93d7e0b92966
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2022-10-20 20:46:46 +0900

    Add files via upload

---
+++ ofApp.cpp
@@ -0,0 +1,84 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+
+	ofSetFrameRate(60);
+	ofSetWindowTitle("openframeworks");
+
+	ofBackground(255);
+	ofSetLineWidth(1.5);
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+	ofSeedRandom(39);
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	ofTranslate(ofGetWidth() * 0.5, ofGetHeight() * 0.5);
+
+	for (int x = -320; x <= 320; x += 20) {
+
+		for (int y = -320; y <= 320; y += 20) {
+
+			auto deg = ofMap(ofNoise(x * 0.002, y * 0.002 + ofGetFrameNum() * 0.015), 0, 1, -360, 360);
+			auto target = glm::vec2(x + cos(deg * DEG_TO_RAD), y + sin(deg * DEG_TO_RAD));
+
+			ofColor fill_color, no_fill_color;
+
+			fill_color = deg > 0 ? ofColor(255) : ofColor(0);
+			no_fill_color = deg > 0 ? ofColor(0) : ofColor(0);
+
+			this->draw_arrow(glm::vec2(x, y), target, 10, fill_color, no_fill_color);
+		}
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::draw_arrow(glm::vec2 location, glm::vec2 target, float size, ofColor fill_color, ofColor no_fill_color) {
+
+	auto angle = std::atan2(target.y - location.y, target.x - location.x);
+
+	ofSetColor(fill_color);
+	ofFill();
+	ofBeginShape();
+	ofVertex(location + glm::vec2(size * cos(angle), size * sin(angle)));
+	ofVertex(location + glm::vec2(size * 0.5 * cos(angle + PI * 0.5), size * 0.5 * sin(angle + PI * 0.5)));
+	ofVertex(location + glm::vec2(size * 0.5 * cos(angle - PI * 0.5), size * 0.5 * sin(angle - PI * 0.5)));
+	ofEndShape();
+
+	ofBeginShape();
+	ofVertex(location + glm::vec2(size * 0.5 * cos(angle + PI * 0.5), size * 0.5 * sin(angle + PI * 0.5)) * 0.25);
+	ofVertex(location + glm::vec2(size * 0.5 * cos(angle + PI * 0.5), size * 0.5 * sin(angle + PI * 0.5)) * 0.25 - glm::vec2(size * cos(angle), size * sin(angle)) * 0.5);
+	ofVertex(location + glm::vec2(size * 0.5 * cos(angle - PI * 0.5), size * 0.5 * sin(angle - PI * 0.5)) * 0.25 - glm::vec2(size * cos(angle), size * sin(angle)) * 0.5);
+	ofVertex(location + glm::vec2(size * 0.5 * cos(angle - PI * 0.5), size * 0.5 * sin(angle - PI * 0.5)) * 0.25);
+	ofEndShape();
+
+	ofSetColor(no_fill_color);
+	ofNoFill();
+	ofBeginShape();
+	ofVertex(location + glm::vec2(size * 0.5 * cos(angle + PI * 0.5), size * 0.5 * sin(angle + PI * 0.5)) * 0.25);
+	ofVertex(location + glm::vec2(size * 0.5 * cos(angle + PI * 0.5), size * 0.5 * sin(angle + PI * 0.5)));
+	ofVertex(location + glm::vec2(size * cos(angle), size * sin(angle)));
+	ofVertex(location + glm::vec2(size * 0.5 * cos(angle - PI * 0.5), size * 0.5 * sin(angle - PI * 0.5)));
+	ofVertex(location + glm::vec2(size * 0.5 * cos(angle - PI * 0.5), size * 0.5 * sin(angle - PI * 0.5)) * 0.25);
+	ofEndShape();
+
+	ofBeginShape();
+	ofVertex(location + glm::vec2(size * 0.5 * cos(angle - PI * 0.5), size * 0.5 * sin(angle - PI * 0.5)) * 0.25);
+	ofVertex(location + glm::vec2(size * 0.5 * cos(angle - PI * 0.5), size * 0.5 * sin(angle - PI * 0.5)) * 0.25 - glm::vec2(size * cos(angle), size * sin(angle)) * 0.5);
+	ofVertex(location + glm::vec2(size * 0.5 * cos(angle + PI * 0.5), size * 0.5 * sin(angle + PI * 0.5)) * 0.25 - glm::vec2(size * cos(angle), size * sin(angle)) * 0.5);
+	ofVertex(location + glm::vec2(size * 0.5 * cos(angle + PI * 0.5), size * 0.5 * sin(angle + PI * 0.5)) * 0.25);
+	ofEndShape();
+}
+
+//--------------------------------------------------------------
+int main() {
+
+	ofSetupOpenGL(720, 720, OF_WINDOW);
+	ofRunApp(new ofApp());
+}
